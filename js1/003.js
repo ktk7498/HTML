809d6e4a1d00348f8ff9f40755a298019f504df5
unknown <romer20@naver.com>
2025-08-08 10:22:28 +0900

    프로젝트 초기화

---
+++ js1/003.js
@@ -0,0 +1,15 @@
+// 2. Type : 데이터의 종류
+console.log(typeof 100);              // number
+console.log(typeof "100");            // string
+console.log(typeof true);             //boolean
+
+// 변수 : 값을 지정하는 것
+let a = 10;
+let b = 20;
+let c = a+b;
+console.log("합계:", c);
+
+let cy = 2025; // cy = current year
+let mby = 2001;  // mby = my birth year
+let d = cy-mby;
+console.log("나이", a);
---
+++ js1/006.js
@@ -0,0 +1,10 @@
+// number, string, boolean, undefined, null, object, ....bigint, symbol
+const saram = {
+    nai:17,
+    irum:"홍길동",
+    tel:"010-1111-2222",
+    address:"인천시 미추홀구 학익동 663-1",
+};
+
+console.log(saram.nai, saram.irum, saram["tel"], saram["address"]);
+console.log(typeof saram);
